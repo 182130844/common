3603a90cc6866b59d1e0f7bc08126b637a2ec7d5
xynws <1092421495@qq.com>
2018-06-27 21:18:10 +0800

    thread base class

--- base_thread.cpp
+++ base_thread.cpp
@@ -1,118 +1,42 @@
+﻿#include <iostream>
+#include <chrono>
 #include "base_thread.h"
-#include <stdio.h>
-#include <stdlib.h>
-#ifdef _WIN32
-#include <process.h>
-#else
-#include <string.h>
-#include <sys/types.h>
-#endif
+#pragma warning(disable:4996)
 
 base_thread::base_thread(const char* name) {
 	strcpy(thread_name, name);
-#ifndef _WIN32
-	pthread_attr_init(&_bt_attr);
-#endif
 }
 
 base_thread::~base_thread() {
-#ifdef _WIN32
-	// close handle
-	for (auto it : _bt_thread_id_list) {
-		CloseHandle(it);
-	}
-#else
-	pthread_attr_destroy(&_bt_attr);
-#endif
+
 }
 
-bool base_thread::activate(int number_of_threads, int stack_size) {
-	if (number_of_threads < 1) {
-		return false;
-	}
-	if (stack_size < 0)
-		stack_size = 0;
-
-	for (int i = 0; i < number_of_threads; i++) {
-
-#ifdef _WIN32
-		unsigned int tid = 0;
-		HANDLE hThread = (HANDLE)_beginthreadex(NULL, stack_size, _bt_proc, this, 0, &tid);
-		if (hThread == NULL) {
-			printf("can't create thread (%d)\n", GetLastError());
-			return false;
-		}
-		Sleep(10);
-		_bt_thread_id_list.push_back(hThread);
-#else
-		pthread_t tid;
-		int err = pthread_create(&tid, &_bt_attr, _bt_proc, this);
-		if (err != 0) {
-			printf("can't create thread (%s)\n", strerror(err));
-			return false;
-		}
-		usleep(10000);
-		_bt_thread_id_list.push_back(tid);
-#endif
+bool base_thread::activate(size_t threads) {
+	std::lock_guard<std::mutex> lock(mutex_);
+	for (size_t i = 0; i < threads; i++) {
+		thread_list_.emplace_back(&base_thread::run, this);
 	}
 	return true;
 }
 
-bool base_thread::kill_all() {
-	auto_lock _al(&_bt_mutex);
-	if (_bt_thread_id_list.empty())
-		return false;
-
-	for (thread_id_iterator it = _bt_thread_id_list.begin(); it != _bt_thread_id_list.end(); it++) {
-#ifdef _WIN32
-		TerminateThread(*it, 0);
-		CloseHandle(*it);
-#else
-		pthread_cancel(*it);
-#endif
+void base_thread::join() {
+	for (auto& thread : thread_list_) {
+		if (thread.joinable()) {
+			thread.join();
+		}
 	}
-	_bt_thread_id_list.clear();
-	return true;
 }
 
-bool base_thread::wait_finish() {
-	auto_lock _al(&_bt_mutex);
-	if (_bt_thread_id_list.empty())
-		return true;
-
-	for (thread_id_iterator it = _bt_thread_id_list.begin(); it != _bt_thread_id_list.end(); it++) {
-#ifdef _WIN32
-		DWORD r = WaitForSingleObject(*it, INFINITE);
-		if (r != 0) {
-			printf("WaitForSingleObject r=%d\n", r);
-		}
-#else
-		int r = pthread_join(*it, 0);
-		if (r != 0) {
-			printf("pthread_join r=%d\n", r);
-		}
-#endif
+void base_thread::run() {
+	try {
+		thread_proc();
 	}
-	_bt_thread_id_list.clear();
-	return true;
+	catch (...) {
+		std::cout << "thread_proc exception: " << thread_name << std::endl;
+	}
 }
 
-#ifdef _WIN32
-unsigned int base_thread::_bt_proc(void* ptr) {
-#else
-void*        base_thread::_bt_proc(void* ptr) {
-#endif
-	base_thread* pthis = reinterpret_cast<base_thread*>(ptr);
-	
-	char name[64] = { 0 };
-	if (pthis != nullptr) {
-		try {
-			strcpy(name, pthis->thread_name);
-			pthis->on_thread_proc();
-		}
-		catch (...) {
-			printf("thread->thread_proc exception (%s)\n", name);
-		}
-	}
-	return 0;
+void base_thread::sleep(int ms)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
--- base_thread.h
+++ base_thread.h
@@ -1,55 +1,23 @@
-#ifndef __base_thread_h__
-#define __base_thread_h__
 
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <pthread.h>
-#include <unistd.h>
-#endif
-
+#pragma once
 #include <list>
-using std::list;
-
-#include "mutex.h"
-
-#ifdef _WIN32
-using thread_id_list = list<HANDLE>;
-#else
-using thread_id_list = list<pthread_t>;
-#endif
-
-using thread_id_iterator = thread_id_list::iterator;
+#include <thread>
+#include <mutex>
 
 class base_thread
 {
 public:
-	base_thread(const char* name);
-	~base_thread();
-
-	virtual int on_thread_proc() = 0;
-
-	bool activate(int number_of_threads = 1, int stack_size = 4 * 1024 * 1024); // default stack size 4Mb
-	bool kill_all();
-	bool wait_finish();
+	base_thread(const char* name = "");
+	virtual ~base_thread();
+	virtual int			thread_proc() = 0;
+	bool				activate(size_t threads = 1);
+	void				join();
+	static void			sleep(int ms);
 
 protected:
-	char thread_name[64];
-
+	char					thread_name[128];
 private:
-
-#ifdef _WIN32
-	static unsigned int __stdcall _bt_proc(void* ptr);
-#else
-	static void*                  _bt_proc(void* ptr);
-#endif
-#ifndef _WIN32
-	pthread_attr_t   _bt_attr;
-#endif
-
-	thread_mutex     _bt_mutex;
-	
-	thread_id_list   _bt_thread_id_list;
-
+	void					run();
+	std::mutex  			mutex_;
+	std::list<std::thread> 	thread_list_;
 };
-#endif // __base_thread_h__
