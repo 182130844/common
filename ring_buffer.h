6dda4ff514fa10119b93b379bf4f71d97d59b4f9
xynws <1092421495@qq.com>
2018-07-20 16:05:20 +0800

    ring buffer

--- ring_buffer.h
+++ ring_buffer.h
@@ -1,40 +1,38 @@
-#ifndef __ring_buffer_h__
-#define __ring_buffer_h__
 
-#include "mutex.h"
-#include <memory>
-class ring_buffer
-{
-public:
-	ring_buffer();
-	~ring_buffer();
+#pragma once
+#include <mutex>
 
-	bool initialize(int init_size, int step_size, int max_size, int block_size);
-	bool finish();
-	void reset();
+namespace shadow {
 
-	bool push_data(void* head, int head_len, void* data, int data_len);
-	bool pop_data(void* head, int& head_len, void* data, int& data_len);
+	class ring_buffer
+	{
+	public:
+		ring_buffer();
+		~ring_buffer();
+		bool init(int init_size, int step_size, int max_size, int max_block_size);
+		bool fini();
+		bool reset();
+		bool putd(void* phead, int headlen, void* pdata, int datalen);
+		bool getd(void* phead, int& headlen, void* pdata, int& datalen);
+		int  get_data_count();
+		int  get_data_size();
 
-	int  get_data_count() const;
-	int  get_data_length() const;
+	private:
+		std::mutex mutex_;
 
-private:
+		char* m_data_ptr;
+		int m_init_size;
+		int m_step_size;
+		int m_max_size;
+		int m_max_buffer_size;
+		int m_max_block_size;
 
-	thread_mutex m_mutex;
+		int m_write_tail;
+		int m_write_head;
+		int m_read_head;
 
-	std::unique_ptr<char[]>  m_data;
+		int m_data_count;
+		int m_data_size;
+	};
 
-	int          m_init_size;
-	int          m_step_size;
-	int          m_max_size;
-	int          m_block_size;
-
-	int          m_write_head;
-	int          m_write_tail;
-	int          m_read_head;
-
-	int          m_data_count;
-	int          m_data_length;
-};
-#endif // __ring_buffer_h__
+}
