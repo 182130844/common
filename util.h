6a5d1fa495b79e6375fa5064c33964e9f5f12ebb
xynws <1092421495@qq.com>
2018-07-20 18:42:05 +0800

    common/util

--- util.h
+++ util.h
@@ -20,26 +20,4 @@ namespace shadow {
 	void time_itos_d(int64_t t, char* out);
 	void time_itos_md(int64_t t, char* out);
 	void time_itostamp(int64_t t, char* out);
-
-	// 10进制转10以下的进制
-	template<typename T, int radix = 8>
-	T conversion(T value) {
-		T ret = value % radix;
-		int i = 0;
-		do {
-			value = value / radix;
-			if (value != 0) {
-				i++;
-
-				T  base = (value % radix);
-				for (int j = 0; j < i; j++) {
-					base *= 10;
-				}
-				ret += base;
-			}
-
-		} while (value >= radix);
-
-		return ret;
-	}
 }
